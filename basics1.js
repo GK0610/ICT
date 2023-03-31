4dcfeb31a369bbeecc033aa765a7c4d3a887bf20
GOPIKRISHNA S <gk120000ks@gmail.com>
2023-03-31 20:17:12 +0530

    Initial commit

---
+++ basics1.js
@@ -0,0 +1,105 @@
+// // a=5;
+// // // var a=8;
+// // console.log(a);
+// // {
+// //     var a=10;
+// //     // console.log(a);
+// // }
+// // let b=10;
+// // // console.log(b);
+// // {
+// //     let b=8;
+// //     // console.log(b);
+// // }
+// // b=20;
+// // // console.log(b);
+// // const pi=3.14
+// // console.log(pi);
+// // {
+// //     const pi=10
+// //     console.log(pi);
+// // }
+// // pi=61
+// // console.log(pi);
+// var a=['meera','nithya',10];
+// console.log(a[4]);
+// a.push('orange')
+// console.log(a);
+// a.pop();
+// console.log(a);
+
+// let car={
+//     model:'i10',
+//     colour:'blue',
+//     brand:'Hyndai'
+// }
+// console.log(car.model);
+
+// let person={
+//     height:'160cm',
+//     weight:'48kg',
+//     age:28,
+// }
+// var person=new Object();
+// person.name="maya";
+// person.age=20;
+// console.log(person);
+
+//Operators
+// var a=2;
+// var name='Jaya';
+// var b=4;
+// var c=a%b;
+// console.log(c);
+
+// var a=2;
+// console.log(a++);
+// console.log(a);
+
+// Comparison Operator 
+// var a=2;
+// var b=10;
+// if (a==b) {
+//     console.log('same');
+// } else if(a>b){
+//     console.log('a is greater');
+// }
+// else{
+//     console.log('b is greater');
+// }
+
+// if(a==b || a==10){
+// console.log('a has a value 10 and equal to b');
+// }
+// else{
+//     console.log('no');
+// }
+
+// Funtions
+// function addt(a,b) {   //Function def
+//     var sum=a+b;
+//     console.log(sum);
+// }
+// addt('neena',7);  //function call
+
+// function multi(a,b) {   //Function def
+//     var mul=a*b;
+//     console.log(mul);
+// }
+// multi(7,6);  //function call
+
+// function Divi(a,b) {   //Function def
+//     var div=a/b;
+//     console.log(div);
+// }
+// Divi(7,6);  //function call
+
+// function addt(a,b) {   //Function def
+//     var sum=a+b;
+//     return sum;
+// }
+// var result=addt(6,7);//function call
+// console.log(result);
+
+// 31 March 2023
+
---
+++ script.js
@@ -0,0 +1,43 @@
+// // console.log("Hello World");
+
+// // // Prints numbers from 0 to 10
+// // for(let i=0;i<=10;i++){
+// //     console.log(i);
+// // }
+// // // Prints numbers from 10 to 0
+// // for(let i=10;i>=0;i--){
+// //     console.log(i);
+// // }
+
+// // // Print sum of first 10 natural numbers
+// // 1+2+3...+10
+
+// // let sum=0
+// // for(let i=1;i<=10;i++){
+// //     sum=sum+i;
+// // }
+// // console.log("Sum of first 10 natural number is " +sum);
+
+// // While Loop
+
+// // let i=0;
+// // while(i<=10){
+// //     console.log(i);
+// //     i++;
+// // }
+
+// // Do-While Loop
+
+// // let i=2;
+// // do {
+// //     console.log(i);
+// // } while (i>2);
+
+// // For...Of Loop
+
+// let students=['Hari','Ram','Shine']
+// for (let student of students) {
+//     console.log(`Hello ${student}`)
+// }
+
+// // Backtick `
