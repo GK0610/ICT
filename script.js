96e6a41c7dcc518567e814524e8c703b4c2ea07f
GOPIKRISHNA S <gk120000ks@gmail.com>
2023-04-04 20:02:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,39 @@
 // }
 
 // // Backtick `
+
+// Challenge 1
+
+for(var i=0; i<=15; i++){
+    if(i=== 0){
+        console.log(i+" is even");
+    }
+    else if(i% 2 === 0){
+        console.log(i+" is even");   
+    }
+    else{
+        console.log(i+" is odd");
+    }
+}
+
+
+// Challenge 2
+for ( var i =1;i<=100;i++)
+{
+  if (i%3===0 && i%5===0)
+  {
+    console.log(i+"FizzBuzz");
+  }
+  else if (i%3===0) 
+  {
+    console.log(i+"Fizz");
+  }
+  else if (i%5===0) 
+  {
+    console.log(i+" Buzz");
+  }
+  else
+  {
+    console.log(i);
+  }
+}
